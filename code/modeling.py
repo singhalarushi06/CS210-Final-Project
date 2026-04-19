928dbc7d59ae3a9d22be622b049e05d7df621266
Arushi Singhal <singhalarushi06@gmail.com>
2026-04-19 15:13:27 -0400

    changes to modeling file (starting with imports and loading df)

---
+++ code/modeling.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (classification_report, confusion_matrix, 
+                             roc_auc_score, roc_curve, accuracy_score,
+                             precision_recall_fscore_support)
+from sklearn.impute import SimpleImputer
+
+import warnings
+warnings.filterwarnings('ignore')
+import joblib
+
+print("NYC VEHICLE COLLISIONS - SEVERITY PREDICTION MODEL")
+
+print("\nLoading dataset...")
+df = pd.read_csv('data\\Collisions_sample_100k.csv', low_memory=False)
+print(f"Dataset loaded with {df.shape[0]:,} rows and {df.shape[1]} columns.")
+
